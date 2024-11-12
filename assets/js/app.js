a9e6324282e89be08a97eafabe14ecda2ccf425d
Rafet Hasanov <rafet.hasanov17@gmail.com>
2024-11-12 16:37:20 +0400

    Hero section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,54 @@
-// Menü açma
+// ! Menu open
 document.querySelector(".bar").addEventListener("click", function (event) {
-    document.querySelector(".menuBar").classList.add("active");
-    event.stopPropagation(); // Tıklama olayını burada durdurarak diğer olaylara geçmesini engelleriz
-  });
+  document.querySelector(".menuBar").classList.add("active");
+  event.stopPropagation();
+});
+
+// ! Menu closed - click the icnon
+document.querySelector(".exit").addEventListener("click", function (event) {
+  document.querySelector(".menuBar").classList.remove("active");
+  event.stopPropagation();
+});
+
+// ! Menu close - click the screen
+document.addEventListener("click", function () {
+  const menu = document.querySelector(".menuBar");
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+  }
+});
+
+document.querySelector(".menuBar").addEventListener("click", function (event) {
+  event.stopPropagation();
+});
+
+// ! Text animation
+
+const element = document.getElementById("typing-text");
+const text = "frontend developer |"; 
+let index = 0;
+let isDeleting = false;
+
+function type() {
+  if (isDeleting) {
+    element.innerHTML = text.substring(0, index--);
+  } else {
+    element.innerHTML = text.substring(0, index++);
+  }
+
   
-  // Menü kapatma - Çıkış ikonuna tıklayınca
-  document.querySelector(".exit").addEventListener("click", function (event) {
-    document.querySelector(".menuBar").classList.remove("active");
-    event.stopPropagation(); // Çıkışa tıklama olayını yayılmasını önler
-  });
+  let speed = isDeleting ? 100 : 200;
+
   
-  // Menü kapatma - Ekranın herhangi bir yerine tıklayınca
-  document.addEventListener("click", function () {
-    const menu = document.querySelector(".menuBar");
-    if (menu.classList.contains("active")) {
-      menu.classList.remove("active");
-    }
-  });
-  
-  // Menü içindeki tıklamaların menüyü kapatmaması için
-  document.querySelector(".menuBar").addEventListener("click", function (event) {
-    event.stopPropagation();
-  });
-  
-  
+  if (!isDeleting && index === text.length) {
+    speed = 2000; 
+    isDeleting = true;
+  } else if (isDeleting && index === 0) {
+    speed = 500; 
+    isDeleting = false;
+  }
+
+  setTimeout(type, speed); 
+}
+
+type(); 
